f81040e52179a26eb6a9efeb3c749745d804baae
ImMamey <32584037+ImMamey@users.noreply.github.com>
2021-06-25 22:39:48 -0400

    ![feat] Part one users
    No description
    
    No ID.

--- proyecto/src/main/java/domain/User.java
+++ proyecto/src/main/java/domain/User.java
@@ -2,6 +2,8 @@ package domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import domain.valueobjects.User_Id;
 import domain.valueobjects.Document;
 import domain.valueobjects.Username;
@@ -14,9 +16,42 @@ public class User {
 
 	private User_Id id; // global identity
 	private Document document;
+	@NotBlank
 	private Username name;
 	private Password password;
 	private Email email;
 	private User_Status status;
 	private Location location;
+
+	public User(@JsonProperty("id") User_Id id, 
+				Document document,
+				@JsonProperty("name") Username name,
+				Password password,
+				Email email,
+				User_Status status,
+				Location location)
+	{
+		super();
+		this.id= id;
+		this.document=document;
+		this.name=name;
+		this.password=password;
+		this.email=email;
+		this.status=status;
+		this.location=location;
+	}
+
+	public User_Id getId()
+	{
+		return id;
+	}
+
+	public Username  getName() {
+        return name;
+    }
+ 
+    public void setName(Username  nombre) {
+        this.name = name;
+    }
+
 }
