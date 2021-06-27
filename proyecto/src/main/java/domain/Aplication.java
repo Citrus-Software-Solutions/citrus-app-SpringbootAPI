5889c02c61a87f42627e1fff2b740a3f4ff40889
marianangelic <marianangelic@gmail.com>
2021-06-26 21:53:39 -0400

    [Feat] Added getters and setters to domain classes
    Added getters and setters
    
    Decoupled the domain from javax.persistence.Entity
    
    Removed unused dependencies
    
    No id

--- proyecto/src/main/java/domain/Aplication.java
+++ proyecto/src/main/java/domain/Aplication.java
@@ -1,6 +1,5 @@
 package domain;
 
-import javax.persistence.Entity;
 
 import domain.valueobjects.Aplication_Id;
 import domain.valueobjects.Aplication_Status;
@@ -9,7 +8,7 @@ import domain.valueobjects.User_Id;
 import domain.valueobjects.Job_Offer_Id;
 
 
-@Entity
+
 public class Aplication {
 
 	private Aplication_Id id; // global identity
@@ -21,6 +20,49 @@ public class Aplication {
 	private User_Id user_id;
 	
 	private Job_Offer_Id job_offer_id;
+	
+	public Aplication_Id getId() {
+		return id;
+	}
+
+	public void setId(Aplication_Id id) {
+		this.id = id;
+	}
+
+	public Aplication_Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Aplication_Status status) {
+		this.status = status;
+	}
+
+	public Date_Format getDate() {
+		return date;
+	}
+
+	public void setDate(Date_Format date) {
+		this.date = date;
+	}
+
+	public User_Id getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(User_Id user_id) {
+		this.user_id = user_id;
+	}
+
+	public Job_Offer_Id getJob_offer_id() {
+		return job_offer_id;
+	}
+
+	public void setJob_offer_id(Job_Offer_Id job_offer_id) {
+		this.job_offer_id = job_offer_id;
+	}
+
+
 }
+
 
 
