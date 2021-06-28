a1225d2d7974a552e0ebc6cb052952fbcb838948
ImMamey <32584037+ImMamey@users.noreply.github.com>
2021-06-27 23:10:08 -0400

    [fix]Table Quialifier
    Added the Table Qualifier to the Class

--- proyecto/src/main/java/com/example/demo/adapter/out/persistance/StaffMemberJpaEntity.java
+++ proyecto/src/main/java/com/example/demo/adapter/out/persistance/StaffMemberJpaEntity.java
@@ -8,11 +8,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.NotEmpty;
 
 @Entity
+@Table(name="Staff_Member")
 public class StaffMemberJpaEntity {
     /*CREATE TABLE Staff_Member (
     id serial PRIMARY KEY,
