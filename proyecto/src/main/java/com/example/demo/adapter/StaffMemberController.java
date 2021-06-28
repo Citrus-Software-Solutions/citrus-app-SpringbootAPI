768b9bef95dbec5b0afb09b61d3ed63e7ae04227
ImMamey <32584037+ImMamey@users.noreply.github.com>
2021-06-28 01:55:56 -0400

    [feat] Staff Members implementation
    added al base classes for staff members:
    
    Controller, and the interface, also connected the entity of staff member to the new classes.
    
    S.M.: no id

---
+++ proyecto/src/main/java/com/example/demo/adapter/StaffMemberController.java
@@ -0,0 +1,37 @@
+package com.example.demo.adapter;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.adapter.out.persistance.StaffMemberJpaEntity;
+import com.example.demo.application.port.IStaffMemberJPA;
+
+@RestController
+@RequestMapping("/StaffMember")
+public class StaffMemberController {
+
+    @Autowired
+    private IStaffMemberJPA staffm;
+    
+    
+	@RequestMapping(value="/view/all", method = RequestMethod.GET)
+	public Iterable<StaffMemberJpaEntity> ListStaffMember(ModelMap sm){
+		return staffm.findAll();
+	}
+    
+	@RequestMapping(value="{id}",method=RequestMethod.GET)
+	public Optional<StaffMemberJpaEntity> FindIdStaffMember(@PathVariable("id") Long id){
+		return staffm.findById(id);
+	}
+	  @RequestMapping(value="", method=RequestMethod.POST)
+		public StaffMemberJpaEntity createStaffMember(@RequestBody StaffMemberJpaEntity staff) {
+			return staffm.save(staff);
+		}
+}
