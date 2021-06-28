b5e0563d9153d44e6dfe279c69ed0614c43dd5d8
Mariosor12 <marioavena120898@gmail.com>
2021-06-28 19:13:48 -0400

    [Feat] Creation of EmployeeJpaEntity, IEmployee repository and EmployeeController
    The Employee Jpa Entity, Employer and Employee Controller repository, was created, where routes are elaborated and then called by other technologies
    
    No id

--- proyecto/src/main/java/com/example/demo/ProyectoApplication.java
+++ proyecto/src/main/java/com/example/demo/ProyectoApplication.java
@@ -15,8 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.adapter.out.persistance.Job_Offer;
-import com.example.demo.application.port.IJob_OfferJPA;
 
 @ComponentScan
 @SpringBootApplication
@@ -26,24 +24,5 @@ public class ProyectoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoApplication.class, args);
-	}
-
-	@Autowired
-    private IJob_OfferJPA jo;
-	
-	@RequestMapping(value="", method = RequestMethod.GET)
-    public Iterable<Job_Offer> ListJob_Offer(ModelMap mp){
-        mp.put("job_offer", jo.findAll());
-        return jo.findAll();
-    }
-    
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-    public Optional<Job_Offer> FindByIdJob_Offer(@PathVariable("id") Long id){
-        return jo.findById(id);
-    }
-    
-    @RequestMapping(value="", method=RequestMethod.POST)
-	public Job_Offer createJob_Offer(@RequestBody Job_Offer job_offer) {
-		return jo.save(job_offer);
 	}
 }
