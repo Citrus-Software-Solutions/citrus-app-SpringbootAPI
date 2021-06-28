c02324e2fbec1b17f608ffaf802531041b50ac9e
Mariosor12 <marioavena120898@gmail.com>
2021-06-27 20:41:57 -0400

    [Feat] Create method post in Job_Offer
    A POST method was placed in the Job_Offer handler
    
    ID E. 4.4.1

--- proyecto/src/main/java/com/example/demo/ProyectoApplication.java
+++ proyecto/src/main/java/com/example/demo/ProyectoApplication.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,16 +29,21 @@ public class ProyectoApplication {
 	}
 
 	@Autowired
-    private IJob_OfferJPA uc;
+    private IJob_OfferJPA jo;
 	
 	@RequestMapping(value="", method = RequestMethod.GET)
     public Iterable<Job_Offer> ListJob_Offer(ModelMap mp){
-        mp.put("job_offer", uc.findAll());
-        return uc.findAll();
+        mp.put("job_offer", jo.findAll());
+        return jo.findAll();
     }
     
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-    public Optional<Job_Offer> nuevo(@PathVariable("id") Long id){
-        return uc.findById(id);
+    public Optional<Job_Offer> FindByIdJob_Offer(@PathVariable("id") Long id){
+        return jo.findById(id);
     }
+    
+    @RequestMapping(value="", method=RequestMethod.POST)
+	public Job_Offer createJob_Offer(@RequestBody Job_Offer job_offer) {
+		return jo.save(job_offer);
+	}
 }
--- proyecto/src/main/java/com/example/demo/adapter/Job_OfferController.java
+++ proyecto/src/main/java/com/example/demo/adapter/Job_OfferController.java
@@ -1,11 +1,15 @@
 package com.example.demo.adapter;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -24,5 +28,14 @@ public class Job_OfferController {
         mp.put("job_offer", jo.findAll());
         return jo.findAll();
     }
-	
+    
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+    public Optional<Job_Offer> FindByIdJob_Offer(@PathVariable("id") Long id){
+        return jo.findById(id);
+    }
+    
+    @RequestMapping(value="", method=RequestMethod.POST)
+	public Job_Offer createJob_Offer(@RequestBody Job_Offer job_offer) {
+		return jo.save(job_offer);
+	}
 }
