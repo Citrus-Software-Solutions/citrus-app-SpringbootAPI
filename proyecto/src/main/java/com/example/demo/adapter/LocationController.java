fe4b1c15890cd04038b40d316e936435c5d2c19b
Mariosor12 <marioavena120898@gmail.com>
2021-06-30 03:30:45 -0400

    [Feat] Multiples packages created from LocationJpa
    Location Jpa entity, ILocation Repository, and Location controller created
    
    No Id

---
+++ proyecto/src/main/java/com/example/demo/adapter/LocationController.java
@@ -0,0 +1,35 @@
+package com.example.demo.adapter;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.adapter.out.persistance.EmployerJpaEntity;
+import com.example.demo.adapter.out.persistance.LocationJpaEntity;
+import com.example.demo.application.port.IEmployerJPA;
+import com.example.demo.application.port.ILocationJPA;
+
+@RestController
+@RequestMapping("api/v1/location")
+public class LocationController {
+
+	@Autowired
+    private ILocationJPA lo;
+	
+    @RequestMapping(value="/view/all", method = RequestMethod.GET)
+    public Iterable<LocationJpaEntity> ListLocation(ModelMap em){
+        return lo.findAll();
+    }
+    
+    @RequestMapping(value="/view/{id}", method = RequestMethod.GET)
+    public Optional<LocationJpaEntity> FindLocation(@PathVariable("id") Long id)
+    {
+        return lo.findById(id);
+    }
+
+}
