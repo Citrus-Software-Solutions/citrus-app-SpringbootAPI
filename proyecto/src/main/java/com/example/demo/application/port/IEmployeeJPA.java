674df43bd3e71a4c55b00eddc62ba3d52df9964f
ImMamey <32584037+ImMamey@users.noreply.github.com>
2021-07-03 19:09:25 -0400

    !Break: Precommit for Hexagonal refactoring
    This commit deletes the content of main, so we could implement the Hexagonal refactoring branch without conflicts.
    
    Id: No id

--- proyecto/src/main/java/com/example/demo/application/port/IEmployeeJPA.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.application.port;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.adapter.out.persistance.EmployeeJpaEntity;
-
-public interface IEmployeeJPA extends JpaRepository<EmployeeJpaEntity, Long>{
-
-}
