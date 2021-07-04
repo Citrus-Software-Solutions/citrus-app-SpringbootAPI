a48f654146e9c97fdc36c9ec781d8a6185268f90
Mariosor12 <marioavena120898@gmail.com>
2021-07-04 18:14:31 -0400

    [Fix] Comments on JobOfferValidator
    A JobOfferValidator function was commented
    
    no id

--- proyecto/src/main/java/com/example/demo/application/port/in/JobApplicationValidation.java
+++ proyecto/src/main/java/com/example/demo/application/port/in/JobApplicationValidation.java
@@ -5,10 +5,10 @@ import com.example.demo.domain.Application;
 
 public class JobApplicationValidation {
 
-	private JobOfferValidation jobOfferValidator = new JobOfferValidation();
+/*	private JobOfferValidation jobOfferValidator = new JobOfferValidation();
 	
 
 	public boolean JobApplicationIsOk(Application jobApplication) {
 		return (jobOfferValidator.isAplicable(jobApplication.getJoboffer()));
-	}
+	}*/
 }
