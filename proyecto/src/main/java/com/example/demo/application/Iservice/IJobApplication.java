49b53b35d6175f6f9a344d8eef2d00afbc345a38
marianangelic <marianangelic@gmail.com>
2021-07-19 02:22:39 -0400

    [Feat] added an ID to employee
    Value object for employee id was created an added to the Employee entity
    
    No ID

--- proyecto/src/main/java/com/example/demo/application/Iservice/IJobApplication.java
+++ proyecto/src/main/java/com/example/demo/application/Iservice/IJobApplication.java
@@ -5,12 +5,13 @@ import java.util.List;
 import com.example.demo.domain.Application;
 import com.example.demo.domain.JobOffer;
 import com.example.demo.domain.valueObjects.Application_Id;
+import com.example.demo.domain.valueObjects.Employee_Id;
 import com.example.demo.infraestructure.database.JPAClasses.ApplicationJPA;
 
 public interface IJobApplication {
 	List<Application> findJobApplication();
 	Application findJobApplication(Application_Id id);
 	Application newJobApplication(ApplicationJPA jobApplication);
-	List<Application> findJobApplication(Integer employee);
+	List<Application> findJobApplication(Employee_Id employee);
 
 }
---
+++ proyecto/src/main/java/com/example/demo/domain/valueObjects/Employee_Id.java
@@ -0,0 +1,15 @@
+package com.example.demo.domain.valueObjects;
+
+public class Employee_Id {
+	
+	
+	public int getValue() {
+		return id;
+	}
+
+	public Employee_Id(int id) {
+		this.id = id;
+	}
+
+	int id;
+}
